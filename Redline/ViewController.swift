8099ca1f25e17bfc5aac11f26d964755aabc315f
Jackson <jhhemme43@gmail.com>
2022-04-26 12:00:41 -0600

    Initial Commit

---
+++ Redline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Redline
+//
+//  Created by Jackson Hemme on 4/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
