a6bd9478780c56669790fe79d9793eb8f3399b06
Jackson <jhhemme43@gmail.com>
2022-05-09 11:57:54 -0600

    almost done baby

--- Redline/ViewController.swift
+++ Redline/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func subcribeButton(_ sender: Any)
+    {
+        
+        let fullURLPath = "https://youtube.com/channel/UCfG_yOpjHL25iQ1YBIPmRIA"
+        
+        let myUrl = URL(string: fullURLPath)
+        let svc = SFSafariViewController(url: myUrl!)
+        svc.delegate = self
+        self.present(svc, animated: true, completion: nil)
+        
+        
+        
+    }
+    
 }
 
